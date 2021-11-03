ed1c045f99fa4e593490e66e7af967173f885012
Yuanyuan Shou <yuanyuan.shou@edu.turkuamk.fi>
2021-11-03 16:50:27 +0200

    First try

---
+++ figure1.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+squares=[1,4,9,16,25]
+fig,ax=plt.subplots()
+ax.plot(squares,linewidth=3)
+ax.set_title("squares",fontsize=24)
+ax.set_xlabel("values",fontsize=14)
+ax.set_ylabel("squares of values",fontsize=14)
+ax.tick_params(axis='both',labelsize=7)
+plt.show()
+
+input_values=[1,2,3,4,5]
+squares=[1,4,9,16,25]
+fig,ax=plt.subplots()
+ax.plot(input_values,squares,linewidth=3)
+ax.set_title("squares",fontsize=24)
+ax.set_xlabel("values",fontsize=14)
+ax.set_ylabel("squares of values",fontsize=14)
+ax.tick_params(axis='both',labelsize=7)
+plt.show()
+
+input_values=[1,2,3,4,5]
+squares=[1,4,9,16,25]
+plt.style.use('fivethirtyeight')
+fig,ax=plt.subplots()
+ax.plot(input_values,squares,linewidth=3)
+ax.set_title("squares",fontsize=24)
+ax.set_xlabel("values",fontsize=14)
+ax.set_ylabel("squares of values",fontsize=14)
+ax.tick_params(axis='both',labelsize=7)
+plt.show()
